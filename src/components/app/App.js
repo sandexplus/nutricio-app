4ea2443584bc11686cd7dbc6eabb198ac6fab14e
Aleksey Plutaev <plutaef@gmail.com>
2022-03-09 14:42:15 +0600

    hashbrowser

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "../login/Login";
 import LoggedMenu from "../pages/LoggedMenu";
