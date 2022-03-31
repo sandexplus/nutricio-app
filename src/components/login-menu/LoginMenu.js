0d3486782625adcb254009cd6610ed4fc88ec253
Evsyukova Ekaterina <evsyukovaekaterina@gmail.com>
2022-03-31 21:30:24 +0300

    about

--- src/components/login-menu/LoginMenu.js
+++ src/components/login-menu/LoginMenu.js
@@ -8,7 +8,7 @@ const LoginMenu = () => {
         <div className="login-menu">
             <div className="container">
                 <Link to="/" className='login-menu__back'>На главную</Link>
-                <h1 className="login-menu__title">Введите ваш логин чтобы войти</h1>
+                <h1 className="login-menu__title">Введите ваш логин, чтобы войти</h1>
                 <input type="text" className="login-menu__login" />
                 <div className="login-menu__b">
                     <Link to="/links" className='login-menu__btn'>Войти</Link>
